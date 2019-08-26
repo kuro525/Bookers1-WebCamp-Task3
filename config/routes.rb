1fffd36b3698c97c5bd5911e00167b8d99286ee2
kuro525 <deep.impact525-2012-5-pc@hotmail.co.jp>
2019-08-26 17:44:33 +0900

    [Finish]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'books#root'
+  #
+  # get 'books' => 'books#index'
+  # get 'books/new' => 'books#new'
+  # post 'books' => 'books#create'
+  #
+  # get 'books/:id' => 'books#show'
+
+  resources :books, onry: [:index, :create, :show ,:edit, :update, :destroy]
+
 end
